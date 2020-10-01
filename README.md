d978f1e22f8f048fbc59e615a332837370cfc0fc
cecile-mc <cecile@womenfirstdigital.org>
2020-10-01 16:56:58 -0500

    theme files added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Women First Digital | Wordpress
+
+This theme is built under the Fressia Theme https://wordpress.org/themes/freesia-empire/
+
+wfd - Use this folder to manage all the assets
+
+page-templates/front-page.php for content of the home page
---
+++ page-templates/front-page.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ * Template Name: Front Page Template
+ *
+ * Displays Corporate template.
+ *
+ * @package Theme Freesia
+ * @subpackage Freesia Empire
+ * @since Freesia Empire 1.0.5
+ */
+?>
+<?php get_header(); ?>
+
+<body data-spy="scroll" data-target="#sidenav" data-offset="100">
+    <!--
+	<div class="">
+		<div class="container">
+			<div class="text-center">
+				<img class="img-fluid" src="images/logo.png" alt="Women First Digital" style="max-width: 100%;">
+			</div>
+
+		</div>
+    </div>
+    -->
+
+	<div class="container-alt">
+		<div class="container">
+			<div class="componant-section">
+				<h2 class="heading light">Our Websites</h2>
+				<div class="panel panel-info">
+					<div class="panel-body">
+						
+						<div class="row">
+							<div class="col-md-4 col-sm-6">
+								<div class="thumbnail thumbnail-caption">
+							      	<a href="https://safe2choose.org" target="_blank">
+							      		<img src="<?php echo get_template_directory_uri(); ?>/wfd/images/s2c.png" alt="100%x180">
+							      	</a>
+							      	<div class="caption">
+								        <h3><a href="https://safe2choose.org" target="_blank">safe2choose.org</a></h3>
+								        <p>
+								        	<ul>
+								        		<li>Free and multilingual counseling that is safe, confidential and free from judgement.</li>
+								        		<li>Worldwide referrals to trustworthy, pro-choice medical providers and organizations.</li>
+								        		<li>A safe place to share abortion experiences.</li>
+								        	</ul>
+								        </p>
+								        <p class="text-center">
+								        	<a href="https://safe2choose.org" target="_blank" class="btn btn-success">Visit!</a>
+    									    <a href="https://www.facebook.com/safe2choose/" target="_blank" class="btn btn-success btn-stroke"><i class="fa fa-facebook"></i></a>
+								        	<a href="https://twitter.com/safe2choose" target="_blank" class="btn btn-success btn-stroke"><i class="fa fa-twitter"></i></a>
+								        	<a href="https://www.instagram.com/safe2choose/" target="_blank" class="btn btn-success btn-stroke"><i class="fa fa-instagram"></i></a>
+								        </p>
+							      	</div>
+							    </div>
+							</div>
+							<div class="col-md-4 col-sm-6">
+								<div class="thumbnail thumbnail-caption">
+							      	<a href="https://findmymethod.org" target="_blank">
+							      		<img src="<?php echo get_template_directory_uri(); ?>/wfd/images/fmm.png" alt="100%x180">
+							      	</a>
+							      	<div class="caption">
+								        <h3><a href="https://findmymethod.org" target="_blank">FindMyMethod.org</a></h3>
+								        <p>
+								        	<ul>
+								        		<li>Comprehensive, easy-to-understand and multilingual contraceptive information.</li>
+								        		<li>Side-by-side comparison and contraceptive finder tools.</li>
+								        		<li>Global and local sexual and reproductive health hub.</li>
+								        	</ul>
+								        </p>
+								        <p class="text-center">
+								        	<a href="https://findmymethod.org" target="_blank" class="btn btn-orange">Visit!</a>
+								        	<a href="https://www.facebook.com/findmymethod" target="_blank" class="btn btn-orange btn-stroke"><i class="fa fa-facebook"></i></a>
+								        	<a href="https://twitter.com/Findmymethod" target="_blank" class="btn btn-orange btn-stroke"><i class="fa fa-twitter"></i></a>
+								        	<a href="https://www.instagram.com/findmymethod/" target="_blank" class="btn btn-orange btn-stroke"><i class="fa fa-instagram"></i></a>
+								        </p>
+							      	</div>
+							    </div>
+							</div>
+							<div class="col-md-4 col-sm-6">
+								<div class="thumbnail thumbnail-caption">
+							      	<a href="https://www.howtouseabortionpill.org" target="_blank">
+							      		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/htu.png" alt="100%x180">
+							      	</a>
+							      	<div class="caption">
+								        <h3><a href="https://www.howtouseabortionpill.org" target="_blank">HowtoUseAbortionPill.org</a></h3>
+								        <p>
+								        	<ul>
+								        		<li>Safe medical abortion instructions and FAQs in 24 languages.</li>
+								        		<li>Local abortion laws, information, and resources organized by country.</li>
+								        		<li>Free abortion training courses for medical providers.</li>
+								        	</ul>
+								        </p>
+								        <p class="text-center">
+								        	<a href="https://www.howtouseabortionpill.org" target="_blank" class="btn btn-purple">Visit!</a>
+								        	<a href="https://www.facebook.com/htuabortionpill/" target="_blank" class="btn btn-purple btn-stroke"><i class="fa fa-facebook"></i></a>
+								        	<a href="https://twitter.com/HTUabortionpill" target="_blank" class="btn btn-purple btn-stroke"><i class="fa fa-twitter"></i></a>
+								        	<a href="https://www.instagram.com/htuabortionpill/" target="_blank" class="btn btn-purple btn-stroke"><i class="fa fa-instagram"></i></a>
+								        	<a href="https://www.youtube.com/channel/UCABqyCQF7d0aZLE9q0O3KhQ?" target="_blank" class="btn btn-purple btn-stroke"><i class="fa fa-youtube"></i></a>
+								        </p>
+							      	</div>
+							    </div>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container-dark">
+		<div class="container">
+			<div class="componant-section">
+				<!-- <h2 class="heading light">Who We Are</h2> -->
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="row">
+							<div class=" col-sm-6 col-md-4">
+								<div class="thumbnail">
+									<img src="<?php echo get_template_directory_uri(); ?>/wfd/images/about-us.png" width="50%" alt="100%x180"/>
+								</div>
+							</div>
+							<div class="col-sm-6 col-md-8">
+								<div class="panel">
+									<div class="panel-heading">
+										<p>
+											Women First Digital (WFD) is a social enterprise that offers comprehensive, practical, inclusive and personalized sexual and reproductive health information and services. WFD has the technological capacity to deliver data-driven and innovative solutions to promote user-centered decision making and improve the well-being of women and youth worldwide.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container" id="impact">
+		<div class="componant-section">
+			<h2 class="heading">Our Impact</h2>
+			<div class="panel panel-primary">
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-4 col-sm-6">
+							<div class="thumbnail thumbnail-caption">
+						      	<img src="<?php echo get_template_directory_uri(); ?>/wfd/images/website-visits.png" alt="100%x180">
+						      	<div class="caption">
+						      		<h2 class="count">13</h2>
+							        <h3>million site visits</h3>
+						      	</div>
+						    </div>
+						</div>
+						<div class="col-md-4 col-sm-6">
+							<div class="thumbnail thumbnail-caption">
+						      	<img src="<?php echo get_template_directory_uri(); ?>/wfd/images/social.png" alt="100%x180">
+						      	<div class="caption">
+						      		<h2 class="count">55</h2>
+							        <h3>million reached via social media</h3>
+						      	</div>
+						    </div>
+						</div>
+						<div class="col-md-4 col-sm-6">
+							<div class="thumbnail thumbnail-caption">
+						      	<img src="<?php echo get_template_directory_uri(); ?>/wfd/images/countries.jpg" alt="100%x180">
+						      	<div class="caption">
+						      		<h2 class="count">180</h2>
+							        <h3>countries and territories</h3>
+						      	</div>
+						    </div>
+						</div>
+
+					</div>
+			
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="container-alt">
+		<div class="container">
+			<div class="componant-section">
+				<h2 class="heading light">Contact Us</h2>
+				<div class="panel panel-info">
+					<div class="panel-body">
+						<div class="row">
+							<div class=" col-sm-6 col-md-4">
+								<div class="thumbnail">
+									<img src="<?php echo get_template_directory_uri(); ?>/wfd/images/contact-us.jpg" alt="100%x180"/>
+								</div>
+							</div>
+							<div class="col-sm-6 col-md-8">
+								<div class="panel">
+									<div class="panel-heading">
+										<p>
+											WFD works globally to reduce the rate of unsafe abortions and broaden contraceptive awareness. We work towards a world in which all women have access to sexual and reproductive health services that are safe, reliable and individually tailored.
+										</p>
+										<p>
+											Contact us - <a href="mailto:info@womenfirstdigital.org">info@womenfirstdigital.org</a>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="container-fluid">
+				<h5 class="text-center">Copyright &copy; 2020 | Women First Digital | All rights reserved.</h5>
+			</div>
+		</div>
+	</div>	
+
+
+
+ <?php get_footer(); ?>
