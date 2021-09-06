d0b9c168a2bb2d4c6b5e9518bd3396b0bcf64f2d
jaibararia <jaibararia@gmail.com>
2021-09-06 20:42:48 +0530

    remove the categories on the posts

--- single.php
+++ single.php
@@ -46,22 +46,23 @@ get_header();
 				<?php $entry_format_meta_blog = $freesiaempire_settings['freesiaempire_entry_meta_blog'];
 				if($entry_format_meta_blog == 'show-meta' ){?>
 					<div class="entry-meta">
+						<!-- Commented script content link, authorname, comment, and tag from here to all the way down -->
 						<?php	
-							if ( current_theme_supports( 'post-formats', $format ) ) {
-								printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
-								sprintf( ''),
-								esc_url( get_post_format_link( $format ) ),
-								get_post_format_string( $format )
-								);
-							} ?>
-						<span class="author vcard"><?php _e('Author :','freesia-empire')?><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php the_author(); ?>">
-						<?php the_author(); ?> </a></span> <span class="posted-on"><?php _e('Date  :','freesia-empire')?><a title="<?php echo esc_attr( get_the_time() ); ?>" href="<?php the_permalink(); ?>">
+							//if ( current_theme_supports( 'post-formats', $format ) ) {
+							//	printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
+							//	sprintf( ''),
+							//	esc_url( get_post_format_link( $format ) ),
+							//	get_post_format_string( $format )
+							//	);
+							//} ?>
+						<!-- <span class="author vcard"><?php //_e('Author :','freesia-empire')?><a href="<?php //echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php //the_author(); ?>">
+						<?php //the_author(); ?> </a></span> --> <span class="posted-on"><?php _e('Date  :','freesia-empire')?><a title="<?php echo esc_attr( get_the_time() ); ?>" href="<?php the_permalink(); ?>">
 						<?php the_time( get_option( 'date_format' ) ); ?> </a></span>
-						<?php if ( comments_open() ) { ?>
-						<span class="comments"><?php _e('Comment  :','freesia-empire')?>
-							<?php comments_popup_link( __( 'No Comments', 'freesia-empire' ), __( '1 Comment', 'freesia-empire' ), __( '% Comments', 'freesia-empire' ), '', __( 'Comments Off', 'freesia-empire' ) ); ?>
-						</span>
-						<?php } ?>
+						<?php //if ( comments_open() ) { ?>
+						<!-- <span class="comments"><?php //_e('Comment  :','freesia-empire')?>
+							<?php //comments_popup_link( __( 'No Comments', 'freesia-empire' ), __( '1 Comment', 'freesia-empire' ), __( '% Comments', 'freesia-empire' ), '', __( 'Comments Off', 'freesia-empire' ) ); ?>
+						</span> -->
+						<?php //} ?>
 					</div> <!-- .entry-meta -->
 				<?php } ?>
 				</header> <!-- .entry-header -->
@@ -80,12 +81,12 @@ get_header();
 					$tag_list = get_the_tag_list( '', __( ' ', 'freesia-empire' ) );
 					$disable_entry_format = $freesiaempire_settings['freesiaempire_entry_format_blog'];
 					if($disable_entry_format =='show' || $disable_entry_format =='show-button' || $disable_entry_format =='hide-button'){ ?>
-					<footer class="entry-footer"> <span class="cat-links">
-						<?php _e('Category : ','freesia-empire');  the_category(', '); ?> </span> <!-- .cat-links -->
-						<?php $tag_list = get_the_tag_list( '', __( ', ', 'freesia-empire' ) );
-							if(!empty($tag_list)){ ?>
-							<span class="tag-links">  <?php   echo $tag_list; ?> </span> <!-- .tag-links -->
-							<?php } ?>
+					<footer class="entry-footer post-footer"> <!-- <span class="cat-links">
+						<?php //_e('Category : ','freesia-empire');  the_category(', '); ?> </span> --> <!-- .cat-links -->
+						<?php //$tag_list = get_the_tag_list( '', __( ', ', 'freesia-empire' ) );
+							//if(!empty($tag_list)){ ?>
+							<!-- <span class="tag-links">  <?php   //echo $tag_list; ?> </span> --> <!-- .tag-links -->
+							<?php //} ?>
 					</footer> <!-- .entry-meta -->
 					<?php }
 				} ?>
